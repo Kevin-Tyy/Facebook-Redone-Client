3c1681e294a473646c52e5b274931cc458dc6e7a
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-09-03 05:07:11 +0200

    Mobile features

--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -6,7 +6,7 @@ const PageLayout = () => {
 	return (
 		<main className="min-h-screen bg-white dark:bg-background-primary">
 			<section className="p-2 2xl:p-0 flex 2xl:justify-center">
-				<section className="w-full sm:pt-2 flex justify-center gap-6">
+				<section className="w-full pt-2 flex justify-center gap-6">
 					<Sidebar />
 					<Outlet />
 					<SideRight />
